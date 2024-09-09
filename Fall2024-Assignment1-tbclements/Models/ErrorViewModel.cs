6c5bb01c7c54ed38ccf4ec4caf2b4ca784f2438d
tbclements <tbclements@crimson.ua.edu>
2024-09-09 12:45:57 -0500

    Create Repo

---
+++ Fall2024-Assignment1-tbclements/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2024_Assignment1_tbclements.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
