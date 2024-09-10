38329b77e6f6f72d62e61a3347a54f48dd6275d1
tbclements <tbclements@crimson.ua.edu>
2024-09-10 09:27:02 -0500

    First Iteration of Site, all components

--- Fall2024-Assignment1-tbclements/wwwroot/js/site.js
+++ Fall2024-Assignment1-tbclements/wwwroot/js/site.js
@@ -2,3 +2,55 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function loadLandingPage(){
+    let firstElems = document.getElementsByClassName("firstPageItem")
+
+    let secondElems = document.getElementsByClassName("secondPageItem")
+    let thirdElems = document.getElementsByClassName("thirdPageItem")
+
+    for(let i = 0; i < firstElems.length; i++){
+        firstElems[i].setAttribute("style", "display: inline");
+    }
+    for(let i = 0; i < secondElems.length; i++){
+        secondElems[i].setAttribute("style", "display: none");
+    }
+    for(let i = 0; i < thirdElems.length; i++){
+        thirdElems[i].setAttribute("style", "display: none");
+    }
+}
+
+function loadTrailer(){
+    let firstElems = document.getElementsByClassName("firstPageItem")
+
+    let secondElems = document.getElementsByClassName("secondPageItem")
+    let thirdElems = document.getElementsByClassName("thirdPageItem")
+
+    for(let i = 0; i < firstElems.length; i++){
+        firstElems[i].setAttribute("style", "display: none");
+    }
+    for(let i = 0; i < secondElems.length; i++){
+        secondElems[i].setAttribute("style", "display: inline");
+    }
+    for(let i = 0; i < thirdElems.length; i++){
+        thirdElems[i].setAttribute("style", "display: none");
+    }
+}
+
+function loadSummary(){
+    let firstElems = document.getElementsByClassName("firstPageItem")
+
+    let secondElems = document.getElementsByClassName("secondPageItem")
+    let thirdElems = document.getElementsByClassName("thirdPageItem")
+
+    for(let i = 0; i < firstElems.length; i++){
+        firstElems[i].setAttribute("style", "display: none");
+    }
+    for(let i = 0; i < secondElems.length; i++){
+        secondElems[i].setAttribute("style", "display: none");
+    }
+    for(let i = 0; i < thirdElems.length; i++){
+        thirdElems[i].setAttribute("style", "display: inline");
+    }
+}
+
+loadSummary();
